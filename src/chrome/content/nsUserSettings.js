bf3ab4d924cfbcb9470ac6a04e76ad77a94ec082
Masahiko Imanaka <chimantaea_mirabilis@yahoo.co.jp>
2018-05-15 22:58:08 +0900

    add import Services.jsm to nsUserSettings.js

--- src/chrome/content/nsUserSettings.js
+++ src/chrome/content/nsUserSettings.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+ChromeUtils.import("resource://gre/modules/Services.jsm");
 // Copy from here:
 // https://hg.mozilla.org/mozilla-central/file/ab1d29e62ee4/toolkit/obsolete/content/nsUserSettings.js
 
